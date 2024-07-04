1def462678f64836bd292fd8876251e55069f58e
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-07-04 19:18:39 -0300

    feat: added delete course to controller

--- forum/src/main/java/com/harcanjo/forum/controllers/CourseController.java
+++ forum/src/main/java/com/harcanjo/forum/controllers/CourseController.java
@@ -3,7 +3,9 @@ package com.harcanjo.forum.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,5 +44,11 @@ public class CourseController {
 	public void updateCourse(@RequestBody @Valid CourseUpdateDTO data) {
 		var course = repository.getReferenceById(data.id());
 		course.updateCourseInformations(data);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void deleteCourse(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
