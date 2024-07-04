1cb1d4b7c6ba19898602571b6f6708939fc2a010
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-07-04 13:50:26 -0300

    feat: added profile jpa entity

---
+++ forum/src/main/java/com/harcanjo/forum/profile/Profile.java
@@ -0,0 +1,26 @@
+package com.harcanjo.forum.profile;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="profiles")
+@Entity(name="Profile")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Profile {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+}
