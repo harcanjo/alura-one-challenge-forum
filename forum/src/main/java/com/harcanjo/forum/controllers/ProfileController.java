1c78a8e9194f586ab3e0c7a6d95e668e373a4189
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-07-04 19:16:23 -0300

    feat: added delete profile to controller

--- forum/src/main/java/com/harcanjo/forum/controllers/ProfileController.java
+++ forum/src/main/java/com/harcanjo/forum/controllers/ProfileController.java
@@ -3,7 +3,9 @@ package com.harcanjo.forum.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,5 +45,10 @@ public class ProfileController {
 		var profile = repository.getReferenceById(data.id());
 		profile.updateProfileInformations(data);
 	}
-
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void deleteProfile(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 }
