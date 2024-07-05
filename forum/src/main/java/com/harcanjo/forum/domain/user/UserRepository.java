3364d030cdff21aafa265d00fa84917e56246f76
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-07-05 17:30:29 -0300

    feat: added security authentication service basic settings

--- forum/src/main/java/com/harcanjo/forum/domain/user/UserRepository.java
+++ forum/src/main/java/com/harcanjo/forum/domain/user/UserRepository.java
@@ -3,9 +3,12 @@ package com.harcanjo.forum.domain.user;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Page<User> findAllByActiveTrue(Pageable page);
+
+	UserDetails findByEmail(String username);
 
 }
