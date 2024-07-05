cda5fed730ab66a1e636ade4eee02f7b8bd59230
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-07-05 15:07:23 -0300

    feat: add api error 400 handler

--- forum/src/main/java/com/harcanjo/forum/infra/ApiErrorHandler.java
+++ forum/src/main/java/com/harcanjo/forum/infra/ApiErrorHandler.java
@@ -1,6 +1,10 @@
 package com.harcanjo.forum.infra;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,4 +18,16 @@ public class ApiErrorHandler {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<ValidationErrorDTO>> handleError400(MethodArgumentNotValidException ex) {
+		var errors = ex.getFieldErrors();
+		
+		return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorDTO::new).toList());
+	}	
+	
+	private record ValidationErrorDTO(String field, String message) {
+		public ValidationErrorDTO(FieldError error) {
+			this(error.getField(), error.getDefaultMessage());
+		}
+	}
 }
