6a7c0ceb2d2612fea970e0f007aad13649c24e15
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-07-09 12:06:41 -0300

    refactor: course update dto

--- forum/src/main/java/com/harcanjo/forum/domain/course/CourseUpdateDTO.java
+++ forum/src/main/java/com/harcanjo/forum/domain/course/CourseUpdateDTO.java
@@ -1,10 +1,6 @@
 package com.harcanjo.forum.domain.course;
 
-import jakarta.validation.constraints.NotNull;
-
 public record CourseUpdateDTO(
-		@NotNull
-		Long id,
 		String name,
 		CourseCategory category
 		) {
